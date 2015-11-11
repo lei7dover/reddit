84c2a7e64429193b1fb1e839b04440da45a6feec
lei7dover <limhoff@wastebits.com>
2015-11-10 19:23:58 -0500

    error routing to usersession delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :votes
   resources :links
+  resources :user_sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
